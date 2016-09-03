71ae2705045550286d0731a37769d861eedb9242
lareinating <lareinating@gmail.com>
2016-09-03 12:08:20 +0800

    fix test case

--- src/test/java/test/LinesProcessorTest.java
+++ src/test/java/test/LinesProcessorTest.java
@@ -11,22 +11,12 @@ public class LinesProcessorTest {
     @Test
     public void testGetProcessLines() {
         LinesProcessor linesProcessor = new LinesProcessor();
-        String[] lines1 = {"tomorrow line", "no line"};
-        String[] lines2 = {"a test", "may day"};
+        String[] lines1 = {"tomorrow line", "no line", "a test", "may day"};
         linesProcessor.addLines(lines1);
         linesProcessor.addLines(lines2);
         String[] res =  linesProcessor.getProcessedLines();
-        assertTrue(res.length == 2);
+        assertTrue(res.length == 4);
         assertEquals("tomorrow line", res[0]);
         assertEquals("may day", res[3]);
-
-        LinesProcessor linesProcessor2 = new LinesProcessor();
-        String[] lines3 = {"Tomorrow line", "no line", "A test", "may Day"};
-        linesProcessor2.addLines(lines3);
-        String[] res2 =  linesProcessor2.getProcessedLines();
-        assertTrue(res2.length == 2);
-        assertEquals("Tomorrow line", res[0]);
-        assertEquals("A Test", res[1]);
-        assertFalse(res[1].equals("no line"));
     }
 }
