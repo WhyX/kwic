a1bf915080fa8d9a9b45a10ffa0ac621bfc94015
lareinating <lareinating@gmail.com>
2016-09-03 11:16:56 +0800

    add test for requiredwords

---
+++ src/test/java/test/RequiredWordsTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.junit.Test;
+import CS3213.RequiredWords;
+
+/**
+ * Created by lareinating on 3/9/16.
+ */
+public class RequiredWordsTest {
+    @Test
+    public void testIsRequiredWord() throws Exception {
+        RequireWords requiredWords = RequiredWords.getRequiredWords();
+
+        assertFalse(requiredWords.isRequiredWord("Tomorrow"));
+
+        requiredWords.addRequiredWord("Tomorrow");
+        assertTrue(requiredWords.isRequiredWord("Tomorrow"));
+        assertTrue(requiredWords.isRequiredWord("tomorrow")); // case insensitive
+        assertFalse(requiredWords.isRequiredWord("Today"));
+
+        requiredWords.addRequiredWord("me");
+        requiredWords.addRequiredWord("tomorrow"); // duplicated
+        requiredWords.removeRequiredWord("tomorrow");
+        assertFalse(requiredWords.isRequiredWord("tomorrow"));
+        assertTrue(requiredWords.isRequiredWord("me"));
+    }
+}
