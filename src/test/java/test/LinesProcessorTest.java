70d4a579548807612ecc82c71509ef3a1cd42f0d
lareinating <lareinating@gmail.com>
2016-09-03 11:33:16 +0800

    add past tense to name

--- src/test/java/test/LinesProcessorTest.java
+++ src/test/java/test/LinesProcessorTest.java
@@ -13,14 +13,14 @@ public class LinesProcessorTest {
         LinesProcessor linesProcessor = new LinesProcessor();
         linesProcessor.addLines(["tomorrow line", "no line"]);
         linesProcessor.addLines(["a test", "may day"]);
-        String[] res =  linesProcessor.getProcessLines();
+        String[] res =  linesProcessor.getProcessedLines();
         assertTrue(res.length == 2);
         assertEquals("tomorrow line", res[0]);
         assertEquals("may day", res[3]);
 
         LinesProcessor linesProcessor2 = new LinesProcessor();
         linesProcessor2.addLines(["Tomorrow line", "no line", "A test", "may Day"]);
-        String[] res2 =  linesProcessor2.getProcessLines();
+        String[] res2 =  linesProcessor2.getProcessedLines();
         assertTrue(res2.length == 2);
         assertEquals("Tomorrow line", res[0]);
         assertEquals("A Test", res[1]);
