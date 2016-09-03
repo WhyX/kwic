255f9d9a5962e68d02c04626e95201ca9c1601de
lareinating <lareinating@gmail.com>
2016-09-03 11:37:33 +0800

    fix one more error

--- src/test/java/test/LinesProcessorTest.java
+++ src/test/java/test/LinesProcessorTest.java
@@ -3,7 +3,7 @@ package test;
 import org.junit.Test;
 import CS3213.LinesProcessor;
 import static org.junit.Assert.*;
-
+import java.util.*;
 /**
  * Created by lareinating on 3/9/16.
  */
@@ -11,15 +11,18 @@ public class LinesProcessorTest {
     @Test
     public void testGetProcessLines() {
         LinesProcessor linesProcessor = new LinesProcessor();
-        linesProcessor.addLines(["tomorrow line", "no line"]);
-        linesProcessor.addLines(["a test", "may day"]);
+        String[] lines1 = {"tomorrow line", "no line"};
+        String[] lines2 = {"a test", "may day"};
+        linesProcessor.addLines(lines1);
+        linesProcessor.addLines(lines2);
         String[] res =  linesProcessor.getProcessedLines();
         assertTrue(res.length == 2);
         assertEquals("tomorrow line", res[0]);
         assertEquals("may day", res[3]);
 
         LinesProcessor linesProcessor2 = new LinesProcessor();
-        linesProcessor2.addLines(["Tomorrow line", "no line", "A test", "may Day"]);
+        String[] lines3 = {"Tomorrow line", "no line", "A test", "may Day"};
+        linesProcessor2.addLines(line3);
         String[] res2 =  linesProcessor2.getProcessedLines();
         assertTrue(res2.length == 2);
         assertEquals("Tomorrow line", res[0]);
