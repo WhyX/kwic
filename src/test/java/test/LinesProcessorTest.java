e7c57cd98440a9a38a784dccf2b2eee8c880b299
lareinating <lareinating@gmail.com>
2016-09-03 11:31:19 +0800

    change param for line processor

--- src/test/java/test/LinesProcessorTest.java
+++ src/test/java/test/LinesProcessorTest.java
@@ -11,20 +11,15 @@ public class LinesProcessorTest {
     @Test
     public void testGetProcessLines() {
         LinesProcessor linesProcessor = new LinesProcessor();
-        linesProcessor.addLines("tomorrow line");
-        linesProcessor.addLines("no line");
-        linesProcessor.addLines("a test");
-        linesProcessor.addLines("may day");
+        linesProcessor.addLines(["tomorrow line", "no line"]);
+        linesProcessor.addLines(["a test", "may day"]);
         String[] res =  linesProcessor.getProcessLines();
         assertTrue(res.length == 2);
         assertEquals("tomorrow line", res[0]);
         assertEquals("may day", res[3]);
 
         LinesProcessor linesProcessor2 = new LinesProcessor();
-        linesProcessor2.addLines("Tomorrow line");
-        linesProcessor2.addLines("no line");
-        linesProcessor2.addLines("A test");
-        linesProcessor2.addLines("may Day");
+        linesProcessor2.addLines(["Tomorrow line", "no line", "A test", "may Day"]);
         String[] res2 =  linesProcessor2.getProcessLines();
         assertTrue(res2.length == 2);
         assertEquals("Tomorrow line", res[0]);
